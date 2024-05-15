853bb26137fd013a721cdbb4ae4660306899ba4a
LUAN HENRIQUE AFONSO <0220482313046@fatectq.local>
2024-05-15 20:21:50 -0300

    chore: project setup

---
+++ main.py
@@ -0,0 +1,15 @@
+import pygame, sys
+
+pygame.init()
+screen = pygame.display.set_mode((400,500))
+clock = pygame.time.Clock()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+            
+    screen.fill((175,215,70))
+    pygame.display.update()
+    clock.tick(60)
